6fdf0388a69c1173c34b81f2d3033856db2283e8
Kordian Kisieliński <dczip89@gmail.com>
2022-11-06 23:39:24 +0100

    Update mod version. Remove tests.

--- src/test/java/RecipeTest.java
+++
@@ -1,9 +0,0 @@
-import dev.prefex.lightestlamp.machine.gascentrifuge.GasCentrifugeRecipe;
-import org.junit.jupiter.api.Test;
-
-public class RecipeTest {
-    @Test
-    public void recipeTest(){
-        new GasCentrifugeRecipe.Serializer();
-    }
-}
